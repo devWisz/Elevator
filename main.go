39153cdaa8050508732ceef0a837deca358b9f7e
USER WisZy <pythondevforuser@gmail.com>
2026-05-11 16:57:33 +0545

    worked on menu case

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"  
+	"net/url"
+	"os"
 )
 
 type DownloadRecord struct {
@@ -11,10 +16,25 @@ type DownloadRecord struct {
 	FileName      string    `json:"file_name"`
 	FileType      string    `json:"file_type"`
 	FileSize      string    `json:"file_size"`
+	OriginalURL   string    `json:"original_url"`
+	LocalLocation string    `json:"local_location"`
+	DownloadedAt  time.Time `json:"downloaded_at"`
 }
 
 
 func main() {
-	fmt.Println("Elevate")
-	fmt.Println("Download Anything Fast")
-}
+	fmt.Println("Elevator | Download anything fast")
+	fmt.Println("Status: The best")
+	fmt.Println(strings.Repeat("-", 45))
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("\nMain Menu:")
+		fmt.Println("1. New Download")
+		fmt.Println("2. View History & Re-download")
+		fmt.Println("3. Clear History")
+		fmt.Println("4. Exit")
+		fmt.Print("\nSelection > ")
+
+		
